06edebf73a24e67cbf0d9a9493d9fe1547e5cee9
frankenstein291294 <frankenstein291294@gmail.com>
2024-09-24 13:33:37 -0600

    firstCommit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+new VenoBox({
+    selector: '.my-image-links',
+    numeration: true,
+    infinigall: true,
+    share: true,
+    spinner: 'rotating-plane',
+
+    bgcolor: '#0EDDA2',
+    border: '5px',
+    toolsBackground: '#0C90D8',
+    toolsColor: '#fff',
+    
+    titleattr: 'data-title',
+    // titlePosition: 'bottom',
+
+    fitView: true,
+    overlayClose: false
+
+});
